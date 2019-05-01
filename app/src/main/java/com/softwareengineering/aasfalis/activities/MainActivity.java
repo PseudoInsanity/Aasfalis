31e25005de94a09de38e8df60436d0a88ee6a777
edmirsuljic <edmir_2@live.se>
2019-05-01 14:51:34 +0200

    fixed location thing

--- app/src/main/java/com/softwareengineering/aasfalis/activities/MainActivity.java
+++ app/src/main/java/com/softwareengineering/aasfalis/activities/MainActivity.java
@@ -85,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        checkUserLocationPermission();
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
