d9b708ca4ebc5e3e251b23c1e7f48ba262361b64
mmadrus <m.madrusan@hotmail.com>
2019-05-22 22:51:22 +0200

    Message - Server Expansion

--- app/src/main/java/com/softwareengineering/aasfalis/client/Database.java
+++ app/src/main/java/com/softwareengineering/aasfalis/client/Database.java
@@ -2,19 +2,15 @@ package com.softwareengineering.aasfalis.client;
 
 import android.support.annotation.NonNull;
 import android.util.Log;
-
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 import com.softwareengineering.aasfalis.models.NewFriend;
-
 import java.util.HashMap;
 import java.util.Map;
 
